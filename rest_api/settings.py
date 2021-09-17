ca17c612ca649311889b5db55c8f0046c34379b5
ichi <tmy12171998@gmail.com>
2021-09-18 00:02:01 +0900

    add host url

--- rest_api/settings.py
+++ rest_api/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nextjs-api-ichi.herokuapp.com']
 
 
 # Application definition
